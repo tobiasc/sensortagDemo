507eb13b21dca2d62097468f3df74b7b483dabcd
Tobias Christensen <tobias@apozy.com>
2015-03-03 07:55:02 -0800

    added new idea site + added more controls

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 It'll run every 200ms and collect temperature, humidity, accelerometer, & gyroscope data. 
 First is discovers the device, then connects to it, and eventually it'll start collecting data from it (usually takes 5-15 seconds).
 
+Initially the printing is turned off, you can toggle it on/off by cliking the left button. The initial printing lower threshold for humidity and temperature is 30% & 25C. You can change these values by clicking the right button, click a single time to increase the lower threshold by 1. Initially it's set to change temperature, but if you click twice within 250 milliseconds it'll switch mode to humidity.
+
 ### How to get started
 1. Install NodeJS (http://nodejs.org/)
 2. Install dependencies `> npm install` 
@@ -15,15 +17,10 @@ Sample Output:
 discover
 connect
 discoverServicesAndCharacteristics
-(0, 0, 0) - (0, 0, 0) - 0 °C - 0 %
-(0, 0, 0) - (0, 0, 0) - 0 °C - 0 %
 notifyHumidity
-(0, 0, 0) - (0, 0, 0) - 0 °C - 0 %
-(0, 0, 0) - (0, 0, 0) - 0 °C - 0 %
 notifyAccelerometer
 notifyGyroscope
-(0, 0, 0) - (0, 0, 0) - 0 °C - 0 %
-(0.047, -1.016, -0.063) - (0, 0, 0) - 0 °C - 0 %
+Printing Enabled
 (0.031, -0.984, 0.25) - (0, 0, 0) - 26.6 °C - 88.4 %
 (-0.016, -0.969, 0.203) - (1.175, 6.18, 3.044) - 26.6 °C - 88.4 %
 (-0.063, -1.141, -0.063) - (1.175, 6.18, 3.044) - 26.6 °C - 88.4 %
@@ -33,3 +30,4 @@ notifyGyroscope
 ### Resources
 - https://github.com/sandeepmistry/node-sensortag
 - https://gist.github.com/sandeepmistry/6075857
+- http://www.instructables.com/ (Ideas)
--- app.js
+++ app.js
@@ -8,13 +8,30 @@ var SensorTag = require('sensortag'),
 		zG: 0, // Z gyroscope
 		humidity: 0, 
 		temperature: 0
-	};
+	},
+	config = {
+		print: false,
+		temperature: {
+			curr: 25,
+			min: 20,
+			max: 30
+		},
+		humidity: {
+			curr: 35,
+			min: 30,
+			max: 50
+		},
+		leftButtonClickMode: 'temperature',
+		leftButtonClickTime: new Date().getTime()
+	}
 
 function printVariables() {
-	console.log('(%d, %d, %d) - (%d, %d, %d) - %d °C - %d %', 
-		sensorData.xA.toFixed(3), sensorData.yA.toFixed(3), sensorData.zA.toFixed(3), 
-		sensorData.xG.toFixed(3), sensorData.yG.toFixed(3), sensorData.zG.toFixed(3), 
-		sensorData.temperature.toFixed(1), sensorData.humidity.toFixed(1));
+	if (config.print && sensorData.temperature > config.temperature.curr && sensorData.humidity > config.humidity.curr) {
+		console.log('(%d, %d, %d) - (%d, %d, %d) - %d °C - %d %', 
+			sensorData.xA.toFixed(3), sensorData.yA.toFixed(3), sensorData.zA.toFixed(3), 
+			sensorData.xG.toFixed(3), sensorData.yG.toFixed(3), sensorData.zG.toFixed(3), 
+			sensorData.temperature.toFixed(1), sensorData.humidity.toFixed(1));
+	}
 }
 
 SensorTag.discover(function(sensorTag) {
@@ -26,9 +43,38 @@ SensorTag.discover(function(sensorTag) {
 
 			setInterval(function () {
 				printVariables();
-			}, 200);
+			}, 500);
 
-			// enable collection of humidity & temparature
+			// enable collection of key presses
+			sensorTag.notifySimpleKey(function() {
+				sensorTag.on('simpleKeyChange', function(left, right) {
+					// left button is only used to turn printing on/off
+					if (left) {
+						config.print = (config.print) ? false : true;
+						if (config.print) {
+							console.log('Printing Enabled');
+						} else {
+							console.log('Printing Disabled');
+						}
+					}
+
+					// right button can be used to toggle between humidity or temperature threshold change
+					if (right) {
+						// if click is within 250 milliseconds, switch click mode
+						var clickTime = new Date().getTime();
+						if ((config.leftButtonClickTime + 250) > clickTime) {
+							config.leftButtonClickMode = (config.leftButtonClickMode === 'temperature') ? 'humidity' : 'temperature';
+							console.log('leftButtonClickMode set to ' + config.leftButtonClickMode);
+						} else {
+							config[config.leftButtonClickMode].curr = (config[config.leftButtonClickMode].curr + 1 <= config[config.leftButtonClickMode].max) ? config[config.leftButtonClickMode].curr + 1 : config[config.leftButtonClickMode].min;
+							console.log(config.leftButtonClickMode + ' Threshold = ' + config[config.leftButtonClickMode].curr);
+						}
+						config.leftButtonClickTime = clickTime;
+					}
+				});
+			});				
+
+			// enable collection of humidity & temperature
 			sensorTag.enableHumidity(function() {
 				sensorTag.on('humidityChange', function(temperature, humidity) {
 					sensorData.temperature = temperature;
